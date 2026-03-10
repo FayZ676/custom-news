0f7408c3b8515d00746e6fbaf214da72538fb1dc
FayZ676 <faizififita1@gmail.com>
2026-03-10 15:18:00 -0700

    Check that we actually get articles from the feeds.

--- backend/tests/app/services/test_ingestion.py
+++ backend/tests/app/services/test_ingestion.py
@@ -7,4 +7,4 @@ from app.models.feed_article import Feed
 @pytest.mark.integration
 @pytest.mark.parametrize("feed", load_feeds(), ids=lambda feed: feed.id)
 def test_get_articles(feed: Feed):
-    get_articles(feed.url)
+    assert len(get_articles(feed.url)) > 0
