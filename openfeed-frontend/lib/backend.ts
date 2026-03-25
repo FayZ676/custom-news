8e2bbd69df97102db5e4ca66c7c76df87e20b5a4
FayZ676 <faizififita1@gmail.com>
2026-03-25 16:27:10 -0700

    Add action to get articles by pages.

--- openfeed-frontend/lib/backend.ts
+++ openfeed-frontend/lib/backend.ts
@@ -1,3 +1,5 @@
+import { Tables } from "./supabase/supabase.types";
+
 export async function updateUserArticleScores(
   userId: string,
   interestId: string,
@@ -19,4 +21,29 @@ export async function updateUserArticleScores(
   if (!res.ok) {
     throw new Error(`Backend API error (${res.status}): ${await res.text()}`);
   }
+}
+
+export async function getGlobalArticlesByPage(
+  page: number,
+  pageSize: number = 20,
+) {
+  const params = new URLSearchParams({
+    page: String(page),
+    page_size: String(pageSize),
+  });
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/global/articles?${params}`,
+    {
+      method: "GET",
+      headers: {
+        "x-api-key": process.env.BACKEND_API_KEY!,
+      },
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error(`Backend API error (${res.status}): ${await res.text()}`);
+  }
+
+  return res.json() as Promise<Tables<"global_articles">[]>;
 }
