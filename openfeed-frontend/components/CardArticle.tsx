4fc3909a2256f4cedf2a32a6eb6fcfd5f382a93c
FayZ676 <faizififita1@gmail.com>
2026-03-31 16:02:39 -0700

    Update font styling.

--- openfeed-frontend/components/CardArticle.tsx
+++ openfeed-frontend/components/CardArticle.tsx
@@ -54,7 +54,9 @@ export function CardArticle({
           className={`font-semibold ${article.is_read ? "line-through text-base-content/40" : "text-base-content"}`}
         >
           {article.global_articles.title}
-          <span className="font-normal text-base-content/70">
+          <span
+            className={`font-normal ${article.is_read ? "line-through text-base-content/40" : "text-base-content/70"}`}
+          >
             , {article.global_articles.feed_title} (
             {timeAgo(article.global_articles.published_at)})
           </span>
