6b038fa15c56f94bba66d451ccaf0551af984877
FayZ676 <faizififita1@gmail.com>
2026-04-01 12:59:56 -0700

    Add email notification cron job.

---
+++ openfeed-database/supabase/migrations/99990000000002_schedule_user_email_notifications.sql
@@ -0,0 +1,19 @@
+-- enable required extensions
+create extension if not exists pg_net;
+create extension if not exists pg_cron;
+
+-- schedule user email notifications to run once a day
+select cron.schedule(
+  'notification_daily',
+  '0 0 * * *',
+  $$
+    select net.http_delete(
+      url := (select decrypted_secret from vault.decrypted_secrets where name = 'backend_url') || '/user/notifications?frequency=daily',
+      headers := jsonb_build_object(
+        'Content-Type', 'application/json',
+        'x-api-key', (select decrypted_secret from vault.decrypted_secrets where name = 'backend_api_key')
+      ),
+      body := '{}'::jsonb
+    );
+  $$
+);
