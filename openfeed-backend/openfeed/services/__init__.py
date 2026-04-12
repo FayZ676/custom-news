f11082ea0eb6d5d7aeabb5c1a68eb55d8d6bf1e7
FayZ676 <faizififita1@gmail.com>
2026-04-12 14:52:29 -0700

    Remove unecessary code.

--- openfeed-backend/openfeed/services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Services layer for business logic and external integrations."""
-
-from .ingestion import fetch_articles, score_articles_for_interests
-
-__all__ = [
-    "fetch_articles",
-    "score_articles_for_interests",
-]
