e8dca0d5961fb986e2a396aa2e8fb5b9b4a970a2
FayZ676 <faizififita1@gmail.com>
2026-05-03 22:14:08 -0700

    Add back checking user settings.

--- openfeed-backend/openfeed/services/notifications.py
+++ openfeed-backend/openfeed/services/notifications.py
@@ -97,11 +97,11 @@ def _build_raw_email(
 
 
 def _get_users_to_notify(db: Client, now_utc: datetime) -> list[PublicUserSettings]:
-    # global_settings = get_global_settings(db)
+    global_settings = get_global_settings(db)
     return [
         u
         for u in get_all_email_notification_users(db)
-        # if _is_in_notification_window(u, now_utc, global_settings.notification_hours)
+        if _is_in_notification_window(u, now_utc, global_settings.notification_hours)
     ]
 
 
