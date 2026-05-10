1f9d59fb61bd2b2fd9acdfd3bea0b52b534cc599
FayZ676 <faizififita1@gmail.com>
2026-05-10 09:36:43 -0700

    Add new user.

--- openfeed-database/supabase/seeds/prod/04_users.sql
+++ openfeed-database/supabase/seeds/prod/04_users.sql
@@ -15,7 +15,8 @@ BEGIN
                 ('00000000-0000-0000-0000-000000000004'::uuid, 'ppendergrass9@gmail.com',   'abc123'),
                 ('00000000-0000-0000-0000-000000000005'::uuid, 'darius.miaji@gmail.com',    'abc123'),
                 ('00000000-0000-0000-0000-000000000006'::uuid, 'mitra@mmengan.com',         'abc123'),
-                ('00000000-0000-0000-0000-000000000007'::uuid, 'alexlhanson@gmail.com',     'abc123')
+                ('00000000-0000-0000-0000-000000000007'::uuid, 'alexlhanson@gmail.com',     'abc123'),
+                ('00000000-0000-0000-0000-000000000008'::uuid, 'yunidude@gmail.com',        'abc123')
         ) AS t(id, email, password)
     LOOP
         IF NOT EXISTS (SELECT 1 FROM auth.users WHERE email = user_data.email) THEN
